85c221dfc3058f1fe3ab61546e1b793451eb0750
docente <docente@172.18.23.48>
2025-05-08 15:55:59 -0500

    Primer commit.

---
+++ gestion-talleres/src/main/java/com/example/ejemplo/GestionTalleresApplication.java
@@ -0,0 +1,15 @@
+package com.example.ejemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionTalleresApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(GestionTalleresApplication.class, args);
+		
+	}
+
+}
