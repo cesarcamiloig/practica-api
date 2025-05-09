75998340068e558eb269fd497c23e001826d5576
cesarcamiloig <cesarcamiloig@gmail.com>
2025-05-08 19:23:20 -0500

    Taller model.

---
+++ gestion-talleres/src/main/java/com/example/demo/model/Taller.java
@@ -0,0 +1,22 @@
+package com.example.demo.model;
+
+import java.util.List;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Taller {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nombre;
+    private String descripción;
+
+    @OneToMany(mappedBy = "taller")
+    private List<Programacion> programaciones;
+}
