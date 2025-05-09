4c71288b24fbc8c6b599792d90c7447c4604a1bf
cesarcamiloig <cesarcamiloig@gmail.com>
2025-05-08 19:25:29 -0500

    Participante model.

---
+++ gestion-talleres/src/main/java/com/example/demo/model/Participante.java
@@ -0,0 +1,31 @@
+package com.example.demo.model;
+
+import java.util.List;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Participante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nombre;
+
+    @ManyToOne
+    @JoinColumn(name = "colegio_id")
+    private Colegio colegio;
+
+    @ManyToOne
+    @JoinColumn(name = "tipo_documento_id")
+    private TipoDocumento tipoDocumento;
+
+    @OneToMany(mappedBy = "participante")
+    private List<Inscripcion> inscripciones;
+
+    @OneToMany(mappedBy = "participante")
+    private List<Asistente> asistencias;
+}
